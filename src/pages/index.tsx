e2086770a63d0b1de8017bbbe2bf3c93f6c07eb3
a7s7h <andrew.subach@gmail.com>
2023-06-21 19:11:32 +0200

    Restyling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,41 @@
 import { useUser } from "@clerk/nextjs";
 import Head from "next/head";
-import Link from "next/link";
-import { api } from "~/utils/api";
+import { LoadingSpinner } from "~/components/loading";
+import { RouterOutputs, api } from "~/utils/api";
 
-export default function Home() {
+type Recipe = RouterOutputs["recipes"]["getAll"][number];
+function RecipeView(props: Recipe) {
+  const recipe = props;
+  return (
+    <div key={recipe.id}>{recipe.content}</div>
+  );
+}
+
+function CreateRecipeWizard() {
   const { isSignedIn, user } = useUser();
   if (!isSignedIn) {
     return null;
   }
-  const { data } = api.recipe.getAll.useQuery();
+
+  return (
+    <div className="flex w-full gap-3">
+      <img src={user.profileImageUrl} className="w-16 h-16 rounded-full"/>
+      <input placeholder="Title" className="bg-transparent grow outline-none"/>
+    </div>
+  );
+}
+
+export default function Home() {
+  const { isSignedIn } = useUser();
+  if (!isSignedIn) {
+    return null;
+  }
+
+  const { data, isLoading } = api.recipes.getAll.useQuery();
+
+  if (isLoading) {
+    return LoadingSpinner();
+  }
 
   return (
     <>
@@ -17,9 +44,11 @@ export default function Home() {
         <meta name="description" content="Ash's best recipes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-lime-100 p-2.5 rounded-xl">
-        <div>Hello { user.firstName }</div>
-        {data?.map((recipe) => <div key={recipe.id}>{recipe.content}</div>)}
+      <aside className="bg-orange-700 p-2.5 mr-2 w-3/12 rounded-xl">
+         {CreateRecipeWizard()}
+      </aside>
+      <main className="bg-slate-700 p-2.5 w-9/12 rounded-xl"> 
+        {data?.map((recipe) => RecipeView(recipe))}
       </main>
     </>
   );
